bcc664f204b5dbc216ed81672667db016dd08b38
f-vasquez <vasquezl.francisco@gmail.com>
2021-05-19 19:35:32 -0400

    modificaciones menores

--- relaciones_vas.py
+++ relaciones_vas.py
@@ -1,22 +1,5 @@
-import numpy as np
-from scipy.stats import bernoulli
-import random
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
-from PIL import Image
 
-# Parámetros para gráficos
-plt.style.use('seaborn-darkgrid')
-
-plt.rcParams.update({'font.size': 25})      
-plt.rc('axes', labelsize=20)
-plt.rc('legend', fontsize=18)  
-plt.rc('xtick', labelsize=20) 
-plt.rc('ytick', labelsize=20) 
-plt.rcParams['axes.titlesize'] = 25
-plt.rcParams["figure.figsize"] = (15,6)
 
 
 st.title('Simulaciones y Conductas Limites')
